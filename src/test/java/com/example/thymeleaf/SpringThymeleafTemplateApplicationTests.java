675c821570bfa02cfbfd768f1ba096295ce7f5b1
Rafał Surdej <special114@mailbox.org>
2024-01-18 20:44:09 +0100

    Enable tests

--- src/test/java/com/example/thymeleaf/SpringThymeleafTemplateApplicationTests.java
+++ src/test/java/com/example/thymeleaf/SpringThymeleafTemplateApplicationTests.java
@@ -1,5 +1,6 @@
 package com.example.thymeleaf;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,11 @@ class SpringThymeleafTemplateApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	public void test1() {
+		Assertions.assertTrue(false);
 	}
 
 }
